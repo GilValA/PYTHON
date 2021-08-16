39bf2fe764ce7dd87b7e5c9c76e59dc54d3bac19
GilValA <87217130+GilValA@users.noreply.github.com>
2021-08-15 21:09:58 -0500

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,13 @@
+def main():
+    #escribe tu código abajo de esta línea
+    print(" Height? ")
+    H = input()
+    print( " Base length? ")
+    L = input()
+    AREA = (float(H) * float(L))/2
+    print (" The area is: " + str(AREA))
+    pass
+
+
+if __name__ == '__main__':
+    main()
